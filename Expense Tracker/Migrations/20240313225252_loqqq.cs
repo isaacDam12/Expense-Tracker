a4399db42e40835169225af67f4f163aa1bc0704
IsaacDamian <ised.dam.mar12@gmail.com>
2024-03-13 18:52:52 -0600

    Agregar archivos de proyecto.

---
+++ Expense Tracker/Migrations/20240313225252_loqqq.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Expense_Tracker.Migrations
+{
+    public partial class loqqq : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Color",
+                table: "Categories",
+                type: "nvarchar(15)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Color",
+                table: "Categories");
+        }
+    }
+}
